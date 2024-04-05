1b94a7cbe4e533bcc41cd8af8c59cec763fa2d25
ariadnarc <ariadr02@ucm.es>
2024-04-05 16:24:51 +0200

    creo que ya esta todo limpio

--- p2/TPV2/TPV2/src/game/DeAcceleration.cpp
+++
@@ -1,7 +0,0 @@
-#include "DeAcceleration.h"
-
-void DeAcceleration::update(Container* o)
-{
-	auto& vel = o->getVel();
-	vel = vel * deAcceleration_; //ira multiplicando la velocidad para q vaya frenanding
-}
