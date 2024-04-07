12b1f3b7eb19f8825b8e29cbdc2ac281ef95f376
pokoli0 <psierr01@ucm.es>
2024-04-07 18:40:19 +0200

    revert pero el pacman no se mueve bien

--- p2/TPV2/TPV2/src/components/ImageWithFrames.h
+++ p2/TPV2/TPV2/src/components/ImageWithFrames.h
@@ -1,40 +1,40 @@
 // This file is part of the course TPV2@UCM - Samir Genaim
 
 #pragma once
+#include <SDL.h>
+
 #include "../ecs/Component.h"
-#include<iostream>
-#include<cstdint>
 
 class Texture;
 class Transform;
 
-class ImageWithFrames: public ecs::Component {
+class ImageWithFrames : public ecs::Component {
 public:
 
 	__CMPID_DECL__(ecs::cmp::IMAGEWITHFRAMES)
 
-	ImageWithFrames(std::string id, int fils, int cols);
+		ImageWithFrames(Texture* tex, //
+			int cols, int rows, //
+			int x, int y, int w, int h, //
+			int scol, int srow, int ncol, int nrow);
 	virtual ~ImageWithFrames();
 	void initComponent() override;
-	void update() override;
 	void render() override;
 
-//private:
-	Texture* myTexture;
-	int fils;
-	int cols;
-
-	int frameWidth;
-	int frameHeight;
-
-	int currentFrame = 26;
-
-	uint32_t lastFrame = 0;
-	uint16_t frameTime = 50;
-
-	Transform* myTransform;
-
-	int firstIndex;
-	int lastIndex;
+private:
+	Texture* tex_;
+	Transform* tr_;
+	int currFrameR_;
+	int currFrameC_;
+	int x_;
+	int y_;
+	int w_;
+	int h_;
+	int srow_;
+	int scol_;
+	int nrow_;
+	int ncol_;
+	int frameWidth_;
+	int frameHeight_;
+	Uint32 lastFrameChange_;
 };
-
--- p2/TPV2/TPV2/src/systems/PacManSystem.cpp
+++ p2/TPV2/TPV2/src/systems/PacManSystem.cpp
@@ -22,27 +22,6 @@ void PacManSystem::initSystem() {
 	// create the PacMan entity
 	//
 	auto pacman = mngr_->addEntity();
-	float healthImageScale = 0.35;
-
-	int fils = 8;
-	int cols = 8;
-
-	int pacmanWidth = 40;
-	int pacmanHeight = 40;
-
-	//aniadir componentes
-	auto trasnformCmp = mngr_->addComponent<Transform>(pacMan);
-	auto imgF_Cmp = mngr_->addComponent<ImageWithFrames>(pacMan, "spriteSheet", fils, cols);
-	//auto healthCmp = mngr_->addComponent<Health>(pacMan, "heart", healthImageScale);
-
-	//healthCmp->setLifes(3);
-
-	trasnformCmp->setHeight(pacmanHeight);
-	trasnformCmp->setWidth(pacmanWidth);
-
-	trasnformCmp->getPos().set((sdlutils().width() - pacmanWidth) / 2,
-		(sdlutils().height() - pacmanHeight) / 2);
-	/*
 	mngr_->setHandler(ecs::hdlr::PACMAN, pacman);
 
 	pmTR_ = mngr_->addComponent<Transform>(pacman);
@@ -51,11 +30,9 @@ void PacManSystem::initSystem() {
 	auto y = (sdlutils().height() - s) / 2.0f;
 	pmTR_->init(Vector2D(x, y), Vector2D(), s, s, 0.0f);
 
-	mngr_->addComponent<ImageWithFrames>(pacman, &sdlutils().images().at("pacman"), 8, 8, 0, 0, 1024/8, 1024/8, 0, 0, 1, 4);
+	mngr_->addComponent<ImageWithFrames>(pacman, &sdlutils().images().at("spriteSheet"), 8, 8, 0, 0, 1024 / 8, 1024 / 8, 0, 0, 1, 4);
 	//mngr_->addComponent<Image>(pacman, &sdlutils().images().at("pacman"));
-	*/
 }
-
 
 void PacManSystem::update() {
 
@@ -72,7 +49,7 @@ void PacManSystem::update() {
 			right = true;
 		}
 		else if (ihldr.isKeyDown(SDL_SCANCODE_LEFT) && !left) { // rotate left
-			
+
 			pmTR_->rot_ -= 90;
 			// also rotate the PacMan so it looks in the same
 			// direction where it moves
@@ -83,10 +60,10 @@ void PacManSystem::update() {
 		else if (ihldr.isKeyDown(SDL_SCANCODE_UP)) { // increase speed
 
 			// empieza a moverse con la rotacion que tenga en el momento
-			pmTR_->getVel().set(0, - speed);
+			pmTR_->getVel().set(0, -speed);
 			pmTR_->getVel() = pmTR_->getVel().rotate(pmTR_->rot_);
 		}
-		else if (ihldr.isKeyDown(SDL_SCANCODE_DOWN)) { 
+		else if (ihldr.isKeyDown(SDL_SCANCODE_DOWN)) {
 
 			// si pulsas downarrow se deja de mover
 			pmTR_->getVel().set(0, 0);
@@ -99,6 +76,7 @@ void PacManSystem::update() {
 		if (ih().isKeyUp(SDL_SCANCODE_LEFT)) {
 			left = false;
 		}
+
 
 	}
 
@@ -125,45 +103,16 @@ void PacManSystem::update() {
 		pmTR_->vel_.set(0.0f, 0.0f);
 	}
 
-	auto imgF_Cmp = mngr_->getComponent<ImageWithFrames>(pacMan);
-
-
-
-	if ((imgF_Cmp->lastFrame + imgF_Cmp->frameTime) < sdlutils().virtualTimer().currTime()) {
-
-		imgF_Cmp->lastFrame = sdlutils().virtualTimer().currTime();
-		imgF_Cmp->currentFrame++;
-		if (imgF_Cmp->currentFrame > imgF_Cmp->lastIndex) {
-			imgF_Cmp->currentFrame = imgF_Cmp->firstIndex;
-		}
-	}
 }
 
-void PacManSystem::recieve(const Message& msg)
+void PacManSystem::recieve(const Message&)
 {
-	if (msg.id == _m_ROUND_START) {
-		//nueva ronda, resetear posicion
-
-	}
-	else if (msg.id == _m_NEW_GAME) {
-		//nueva partida resetear vidas
-
-	}
 }
 
 void PacManSystem::resetPos()
 {
-	auto pmTR_ = mngr_->getComponent<Transform>(pacMan);
-
-	pmTR_->getPos().set((sdlutils().width() - pacmanWidth) / 2,
-		(sdlutils().height() - pacmanHeight) / 2);
-
-	//resetea la velocidad y la rotacion
-	pmTR_->vel_.set(0, 0);
-	pmTR_->rot_ = 0;
 }
 
 void PacManSystem::resetLifes()
 {
-	//esto pa cuando tengamos el health component
 }
